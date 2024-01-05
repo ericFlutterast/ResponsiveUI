6af916c98083d680ce665f5ca339a9924eaca160
Эрик Нугзарович <eriknugzarovic@MacBook-Pro-Erik.local>
2024-01-05 12:59:08 +0300

    init git

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AdaptiveDisign());
+}
+
+class AdaptiveDisign extends StatelessWidget {
+  const AdaptiveDisign({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Hello World'),
+      ),
+    );
+  }
+}
