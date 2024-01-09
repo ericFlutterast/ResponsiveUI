8840561dbdf3b165a047baf428c0a8a336a15d46
Эрик Нугзарович <eriknugzarovic@MacBook-Pro-Erik.local>
2024-01-09 09:53:30 +0300

    cc

--- lib/src/modules/main_screen/presentation/screens/dashboard_screen.dart
+++ lib/src/modules/main_screen/presentation/screens/dashboard_screen.dart
@@ -10,12 +10,31 @@ class DashboardScreen extends StatelessWidget {
     return SafeArea(
       child: SingleChildScrollView(
         child: Padding(
-            padding: EdgeInsets.all(CustomTheme.contentPadding),
-            child: const Column(
-              children: [
-                Header(),
-              ],
-            )),
+          padding: EdgeInsets.all(CustomTheme.contentPadding),
+          child: Column(
+            children: [
+              const Header(),
+              SizedBox(height: CustomTheme.contentPadding),
+              Row(
+                children: [
+                  Expanded(
+                      flex: 5,
+                      child: Container(
+                        height: 500,
+                        color: Colors.white,
+                      )),
+                  SizedBox(width: CustomTheme.contentPadding),
+                  Expanded(
+                      flex: 2,
+                      child: Container(
+                        height: 500,
+                        color: Colors.pink,
+                      )),
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
